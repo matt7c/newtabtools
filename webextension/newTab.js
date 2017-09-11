9e5a04c4cb417b5118451e77584dca0dc145dc01
Geoff Lankow <geoff@darktrojan.net>
2017-09-12 11:48:02 +1200

    Changes needed to make this run in Chromium

--- webextension/newTab.js
+++ webextension/newTab.js
@@ -805,7 +805,7 @@ var newTabTools = {
 		}
 
 		if (this.optionsFilterHostAutocomplete.childElementCount === 0) {
-			chrome.topSites.get({ providers: ['places'] }, sites => {
+			chrome.topSites.get(sites => {
 				for (let s of sites.reduce((carry, site) => {
 					let {protocol, host} = new URL(site.url);
 					if (host && ['http:', 'https:', 'ftp:'].includes(protocol) && !carry.includes(host)) {
@@ -839,7 +839,17 @@ var newTabTools = {
 			n.parentNode.insertBefore(document.createTextNode(newTabTools.getString(n.dataset.label)), n.nextSibling);
 		});
 
-		Prefs.init().then(function() {
+		Promise.all([
+			Prefs.init(),
+			initDB()
+		]).then(function() {
+			if (initDB.waitingQueue) {
+				for (let waitingResolve of initDB.waitingQueue) {
+					waitingResolve.call();
+				}
+				delete initDB.waitingQueue;
+			}
+		}).then(function() {
 			// Everything is loaded. Initialize the New Tab Page.
 			Page.init();
 			newTabTools.updateUI();
@@ -863,29 +873,6 @@ var newTabTools = {
 		}).catch(console.error);
 	},
 	getThumbnails: function() {
-		chrome.runtime.sendMessage({
-			name: 'Thumbnails.get',
-			urls: Grid.sites.filter(s => s && !s.thumbnail.style.backgroundImage).map(s => s.link.url)
-		}, function(thumbs) {
-			Grid.sites.forEach(s => {
-				if (!s) {
-					return;
-				}
-				let link = s.link;
-				if (!link.image) {
-					let thumb = thumbs.get(link.url);
-					if (thumb) {
-						let css = 'url(' + URL.createObjectURL(thumb) + ')';
-						s.thumbnail.style.backgroundImage = css;
-
-						if (newTabTools.selectedSite == s) {
-							newTabTools.siteThumbnail.style.backgroundImage = css;
-							newTabTools.saveCurrentThumbButton.disabled = false;
-						}
-					}
-				}
-			});
-		});
 	}
 };
 
@@ -981,8 +968,6 @@ var newTabTools = {
 	newTabTools.optionsFilterHost.oninput = newTabTools.optionsFilterCount.oninput = function() {
 		newTabTools.optionsFilterSet.disabled = !newTabTools.optionsFilterHost.checkValidity() || !newTabTools.optionsFilterCount.checkValidity();
 	};
-	document.body.oncontextmenu = newTabTools.contextMenuShowing;
-	newTabTools.contextMenu.onclick = newTabTools.contextMenuOnClick;
 
 	window.addEventListener('keydown', function(event) {
 		if (event.key == 'Escape') {
